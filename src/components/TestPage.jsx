2faf3632963f48acc87be1e219fb6d5546657e39
Kjetil  Overby <kjeoever@msn.com>
2021-09-29 20:53:44 +0200

    first commit

---
+++ src/components/TestPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import css from "../../styles/TestPage.module.css";
+
+const TestPage = () => {
+  return (
+    <>
+      <div className={css.container}>
+        <div className={`${css.box1} ${css.box}`}>
+          <h1>X-plore</h1>
+        </div>
+        <div className={`${css.box2} ${css.box}`}></div>
+        <div className={`${css.box3} ${css.box}`}></div>
+        <div className={`${css.box4} ${css.box}`}></div>
+        <div className={`${css.box5} ${css.box}`}></div>
+        <div className={`${css.box6} ${css.box}`}></div>
+        <div className={`${css.box7} ${css.box}`}></div>
+        <div className={`${css.box8} ${css.box}`}></div>
+      </div>
+    </>
+  );
+};
+
+export default TestPage;
